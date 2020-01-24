6619f2999d3d5d86bc322590aefb1902d420e2af
Sunha <shinsun@sheridancollege.ca>
2020-01-24 11:45:50 -0500

    Student file only

---
+++ src/ca/sheridancollege/softwarefundamentals/week1/PartTimeStudent.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.softwarefundamentals.week1;
+
+/**
+ *
+ * @author sunha
+ */
+public class PartTimeStudent extends Student {
+    
+    private int hourOfStudy;
+    public PartTimeStudent(String givenName, String givenID) {
+        super(givenName, givenID);
+    }
+}
